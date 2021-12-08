2e7536b8d03a610538e9f7a9a7477e543d9ebcae
Timur <thedickst@gmail.com>
2021-12-08 11:10:18 +0300

    -add -addNewBlock -broadcast -news

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -72,6 +72,30 @@ $('.gallery__slider').slick({
   ]
 });
 
+$('.news__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button type="button" class="slick-prev-gallery"></button>',
+  nextArrow: '<button type="button" class="slick-next-gallery"></button>',
+  responsive: [
+    {
+      breakpoint: 820,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+});
 
 $("#about-click").on("click", function (event) {
   event.preventDefault();
