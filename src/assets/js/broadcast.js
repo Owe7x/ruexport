8a7353b1f1e749285ce62ba8d6e4057d2b2c6834
Timur <thedickst@gmail.com>
2021-12-10 01:14:37 +0300

    -fix

--- src/assets/js/broadcast.js
+++ src/assets/js/broadcast.js
@@ -1,50 +1,43 @@
 const boradcast = [
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/izirzp?key=&chat_token=MytiVmNFd2xNM3psWjZRYTdWL3krdUlucUVRcVRVSDE3WEdkaVRTV0ZUVT1TSWIzZGFUVE1EemVVQkUz',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Конгресс зал',
         id: 1001
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/ktgbn7?key=&chat_token=My9BNHVrcVlpcU8xZXR4cCtGU2xPZ2lCckNqbkl5WWdOQm5oVnM1WDFiaz1aUEpQdTlHZHh0N0hKOVky',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Пресс зал',
         id: 1002
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/wp6a3v?key=&chat_token=cDlPUHFtQlhwUFRWcXFzelZ2V1F5SmtNZ2dKcGNHU1ZxQnR5N0ZNYjR5RT1zWXJqRUVoZUdjZG80ZW1i',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Зал Валдай',
         id: 1003
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/mz030b?key=&chat_token=MHI2SGVsbDlqVDB1MnVDQXFNWExBcFpYSG1hNUZrb2cyNnpKaDk4d3hvYz1FSWN6Y0s0OW9nVnRpb1hV',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Зал Селигер',
         id: 1004
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/tn3fp0?key=&chat_token=MWIrZGJ2R2xKMTZiYm9mUXVtZVczK2RhbVl1eWJIaWU1Q2V6aWFqL1lGZz1kUXBRVEtjb3NUck1SbnRx',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Зал Байкал',
         id: 1005
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/ex7ev4?key=&chat_token=aXFUaEpmQzJCaXd4Z3NHN3lPOU5kbnVGdWFUWndPZXFsT2xzY3hVS2ptcz1IT1Ixc05hQXVxenpTdFNV',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Зал Иртыш',
         id: 1006
     },
     {
-        src: 'https://facecast.net/v/58dbd0',
+        src: 'https://facecast.net/v/ayhieh?key=&chat_token=dnFvd0ZOQVdydkVRQmlKdGNIc2JGNVhxQVE0YWxVL2pXWHBLOUQ2bUlkYz1WWFFoVFpDdDU2NXFHWmZZ',
         text: 'Трансляция скоро будет доступна',
-        href: '/',
         hrefText: 'Зал Печора',
         id: 1007
     }
@@ -56,7 +49,6 @@ class CardBroadcast {
     constructor(item, template) {
         this.src = item.src;
         this.text = item.text;
-        this.href = item.href;
         this.hrefText = item.hrefText;
         this._template = template;
 
@@ -72,13 +64,11 @@ class CardBroadcast {
         this._element = this._getTemplate();
         this._elementSrc = this._element.querySelector('.broadcast__video-player');
         this._elementText = this._element.querySelector('.broadcast__video-title');
-        this._elementHref = this._element.querySelector('.broadcast__href');
         this._elementHrefText = this._element.querySelector('.broadcast__href');
 
 
         this._elementSrc.src = this.src
         this._elementText.textContent = this.text
-        this._elementHref.src = this.href
         this._elementHrefText.textContent = this.hrefText
         console.log(this._element);
         return this._element;
