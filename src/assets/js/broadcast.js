7685aebfa5db010bf4d7899782b22fcc24ab9344
Timur <thedickst@gmail.com>
2021-12-16 11:45:39 +0300

    -add -addNewElem -broadcast

--- src/assets/js/broadcast.js
+++ src/assets/js/broadcast.js
@@ -1,37 +1,69 @@
 const boradcast = [
     {
-        src: 'https://facecast.net/v/izirzp?key=&chat_token=MytiVmNFd2xNM3psWjZRYTdWL3krdUlucUVRcVRVSDE3WEdkaVRTV0ZUVT1TSWIzZGFUVE1EemVVQkUz',
+        src: 'https://www.youtube.com/embed/iElswNoogds',
         hrefText: 'Конгресс зал',
+        arrayElem: [
+            {name: 'НАГРАЖДЕНИЕ ПОБЕДИТЕЛЕЙ ВСЕРОССИЙСКОГО КОНКУРСА «ЭКСПОРТЕР ГОДА»' , src: 'https://facecast.net/v/izirzp?key=&chat_token=MytiVmNFd2xNM3psWjZRYTdWL3krdUlucUVRcVRVSDE3WEdkaVRTV0ZUVT1TSWIzZGFUVE1EemVVQkUz' },
+            {name: 'ПЛЕНАРНАЯ СЕССИЯ «2030 – УЖЕ ЗАВТРА. ЧТО ДЕЛАТЬ?»' , src: 'https://www.youtube.com/embed/iElswNoogds' },
+            {name: 'ЭКСПОРТНЫЙ ФОРУМ РСПП «СОЗДАВАЯ НОВУЮ РЕАЛЬНОСТЬ: НОВЫЕ РЫНКИ, ПАРТНЕРЫ И МЕХАНИЗМЫ ПОДДЕРЖКИ СО СТОРОНЫ ГОСУДАРСТВА И БИЗНЕСА»' , src: 'https://www.youtube.com/embed/jIbQmrSvPL8'},
+            {name: 'ЧТО РОССИЯ ГОТОВА ПРЕДЛОЖИТЬ МИРУ В СФЕРЕ E-COMMERCE?' , src: 'https://www.youtube.com/embed/opwYLuEL6es' }
+        ],
         id: 1001
     },
     {
-        src: 'https://facecast.net/v/ktgbn7?key=&chat_token=My9BNHVrcVlpcU8xZXR4cCtGU2xPZ2lCckNqbkl5WWdOQm5oVnM1WDFiaz1aUEpQdTlHZHh0N0hKOVky',
+        src: 'https://www.youtube.com/embed/Pj8VysKj3co',
         hrefText: 'Пресс зал',
+        arrayElem: [
+            {name: 'СЕССИЯ "ИНФРАСТРУКТУРНЫЕ ПРОЕКТЫ КАК ДРАЙВЕР РОСТА ВЫСОКОТЕХНОЛОГИЧНОГО ЭКСПОРТА"' , src: 'https://www.youtube.com/embed/Pj8VysKj3co'},
+            {name: 'СЕССИЯ "10ЛЕТ ФИНАНСИРОВАНИЯ ЭКСПОРТА: УРОКИ И ПЕРСПЕКТИВЫ"' , src: 'https://www.youtube.com/embed/5N-2Q7LXClQ' },
+        ],
         id: 1002
     },
     {
         src: 'https://player.vimeo.com/video/656536557?h=408def4a07&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479',
         hrefText: 'Зал Валдай',
+        arrayElem: [
+            {name: 'СТРАТЕГИЧЕСКАЯ СЕССИЯ «РАЗВИТИЕ ЭКСПОРТА - РАЗВИТИЕ ТЕРРИТОРИИ»' , src: 'https://player.vimeo.com/video/656536557?h=408def4a07&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479' },
+            {name: 'СЕССИЯ "НОВЫЙ КЛИЕНТСКИЙ ПУТЬ ЭКСПОРТЕРА: ПЕРВЫЕ ШАГИ"' , src: 'https://player.vimeo.com/video/656536557?h=408def4a07&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479' },
+        ],
         id: 1003
     },
     {
-        src: 'https://facecast.net/v/mz030b?key=&chat_token=MHI2SGVsbDlqVDB1MnVDQXFNWExBcFpYSG1hNUZrb2cyNnpKaDk4d3hvYz1FSWN6Y0s0OW9nVnRpb1hV',
+        src: 'https://www.youtube.com/embed/AZ8qkQ3kBls',
         hrefText: 'Зал Селигер',
+        arrayElem: [
+            {name: 'СЕССИЯ «ОБЩЕМИРОВОЙ ТРЕНД НА ДЕКАРБОНИЗАЦИЮ КАК СТИМУЛ ДЛЯ ТРАНСФОРМАЦИИ ЭКСПОРТНО ОРИЕНТИРОВАННЫХ ПРОИЗВОДСТВ»' , src: 'https://www.youtube.com/embed/AZ8qkQ3kBls'},
+            {name: 'СЕССИЯ «РАСШИРЕНИЕ ЛОГИСТИЧЕСКИХ КОРИДОРОВ КАК КЛЮЧЕВОЙ ЭЛЕМЕНТ ПРОДВИЖЕНИЯ ЭКСПОРТНЫХ ТОВАРОВ»' , src: 'https://www.youtube.com/embed/HxobA_brb1A'},
+        ],
         id: 1004
     },
     {
         src: 'https://facecast.net/v/tn3fp0?key=&chat_token=MWIrZGJ2R2xKMTZiYm9mUXVtZVczK2RhbVl1eWJIaWU1Q2V6aWFqL1lGZz1kUXBRVEtjb3NUck1SbnRx',
         hrefText: 'Зал Байкал',
+        arrayElem: [
+            {name: 'ВЕБИНАР ПО МЕРАМ ГОСПОДДЕРЖКИ' , src: 'https://facecast.net/v/tn3fp0?key=&chat_token=MWIrZGJ2R2xKMTZiYm9mUXVtZVczK2RhbVl1eWJIaWU1Q2V6aWFqL1lGZz1kUXBRVEtjb3NUck1SbnRx' },
+            {name: 'ВЕБИНАР «SHOWCASE ПЛАТФОРМЫ ПРОФЕССИОНАЛЫ ЭКСПОРТА»' , src: 'https://www.youtube.com/embed/47V2WnVU2s4'},
+            {name: 'СЕССИЯ «МЕЖДУНАРОДНЫЙ КОММЕРЧЕСКИЙ АРБИТРАЖНЫЙ СУД И МОРСКАЯ АРБИТРАЖНАЯ КОМИССИЯ ПРИ ТПП РОССИИ КАК МЕХАНИЗМЫ ЗАЩИТЫ ПРАВ РОССИЙСКИХ ЭКСПОРТЕРОВ»' , src: 'https://www.youtube.com/embed/04mCM4uT_-0'},
+        ],
         id: 1005
     },
     {
-        src: 'https://facecast.net/v/ex7ev4?key=&chat_token=aXFUaEpmQzJCaXd4Z3NHN3lPOU5kbnVGdWFUWndPZXFsT2xzY3hVS2ptcz1IT1Ixc05hQXVxenpTdFNV',
+        src: 'https://www.youtube.com/embed/zR2kG4Vh9AU',
         hrefText: 'Зал Иртыш',
+        arrayElem: [
+            {name: 'МАСТЕР-КЛАСС-ТУРНИР ПО ПЕРЕГОВОРАМ ЭКСПОРТЕРОВ С ИМПОРТЕРАМИ (С ВУЗАМИ-ПАРТНЕРАМИ) ' , src: 'https://www.youtube.com/embed/zR2kG4Vh9AU'},
+            {name: 'СЕССИЯ "ЭКСПОРТ РОССИЙСКОГО ОБРАЗОВАНИЯ" ' , src: 'https://facecast.net/v/ex7ev4?key=&chat_token=aXFUaEpmQzJCaXd4Z3NHN3lPOU5kbnVGdWFUWndPZXFsT2xzY3hVS2ptcz1IT1Ixc05hQXVxenpTdFNV'},
+        ],
         id: 1006
     },
     {
-        src: 'https://facecast.net/v/ayhieh?key=&chat_token=dnFvd0ZOQVdydkVRQmlKdGNIc2JGNVhxQVE0YWxVL2pXWHBLOUQ2bUlkYz1WWFFoVFpDdDU2NXFHWmZZ',
+        src: 'https://www.youtube.com/embed/SekLF1T4mEk',
         hrefText: 'Зал Печора',
+        arrayElem: [
+            {name: 'ВЕБИНАР «РЕГУЛЯТОРНЫЕ АСПЕКТЫ ЭКСПОРТА ПО КАНАЛАМ ИНТЕРНЕТ-ТОРГОВЛИ»' , src: 'https://www.youtube.com/embed/SekLF1T4mEk'},
+            {name: 'МАСТЕР-КЛАСС ПРОГРАММЫ МИНИ-MBA «ЭКСПОРТНЫЙ СТАНДАРТ ДЛЯ МСП»: MARKETING & SALES В ЭКСПОРТЕ' , src: 'https://www.youtube.com/embed/pxHYpcTODNc'},
+            {name: 'ВЕБИНАР "ЗАЧЕМ ПЕРЕВОДИТЬ ЭКСПОРТ В ЦИФРУ"' , src: 'https://www.youtube.com/embed/dwc37mM5sX0'},
+        ],
         id: 1007
     },
     {
@@ -77,8 +109,8 @@ class CardBroadcast {
     constructor(item, template) {
         this.src = item.src;
         this.hrefText = item.hrefText;
+        this.arrayElem = item.arrayElem
         this._template = template;
-
     }
     _getTemplate() {
         const element = document
@@ -87,38 +119,64 @@ class CardBroadcast {
             .cloneNode(true)
         return element;
     }
+    _getTempalteGroup() {
+        const elementGroup = document
+            .querySelector('#broadcast__group')
+            .content
+            .cloneNode(true)
+        return elementGroup;
+    }
     createElement() {
+
         this._element = this._getTemplate();
         this._elementSrc = this._element.querySelector('.broadcast__video-player');
         this._elementHrefText = this._element.querySelector('.broadcast__href');
-
+        this._elemGroup = this._element.querySelector('.broadcast__group-info');
 
         this._elementSrc.src = this.src
         this._elementHrefText.textContent = this.hrefText
-        console.log(this._element);
+        this.createGroup()
+
         return this._element;
+    }
+    createGroup() {
+
+
+        this.arrayElem.forEach((elem) => {
+            console.log(elem);
+            this._elemGroupInfo = this._getTempalteGroup()
+            this._elemGroupName = this._elemGroupInfo.querySelector('.broadcast__name');
+            this._elemGroupSrc = this._elemGroupInfo.querySelector('.broadcast__btn');
+            
+            this._elemGroupName.textContent = elem.name
+            this._elemGroupSrc.href = elem.src
+            this._elemGroup.append(this._elemGroupInfo);
+        })
+        return this._elemGroup
     }
 }
 
 $('.broadcast__min-btn').on('click', function(e) {
     $('.broadcast__video').remove()
     const target = e.target
-    console.log(target.parentNode);
     $('.broadcast__min-block').removeClass('min__block-active')
     $(target.parentNode).addClass('min__block-active')
     const targetId = target.id
-    console.log(targetId);
     boradcast.forEach(item => {
         if(item.id == targetId) {
             addCardBroadcast(item, elementsBroadcast, '#element-template-broadcast')
         } 
     })
 })
+$('.broadcast__video-result').on('click', function(e) {
+    e.preventDefault()
+    const target = e.target
+    $('.broadcast__video-player')[0].src = target.href
+    console.log($('.broadcast__video-player')[0]);
+})
 
 function addCardBroadcast(item ,elemList, elemTemplate) {
-    console.log(item);
     const card = new CardBroadcast(item, elemTemplate);
     const cardElements = card.createElement();
-    console.log(cardElements);
     elemList.prepend(cardElements);
 }
